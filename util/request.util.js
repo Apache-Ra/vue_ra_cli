df05f6291e252ba81b4647f7469701c930d4a5f7
Apache-Ra <summer.vip.20@qq.com>
2020-02-20 13:27:35 +0800

    新增工具类

---
+++ util/request.util.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ *     请求类
+ * 
+ **/
+const REQUEST = () =>{
+
+}
+
+const SERVER_NAME = window.location.hostname == 'localhost'?'':'';
+const DOME_NAME = '';
+
+export default {REQUEST, SERVER_NAME, DOME_NAME}
